fe7a457967ec32705724e32baf913a27f6c3cd3f
ahmadziadev <ziamain@proton.me>
2026-04-22 13:07:10 +0500

    Create function to generate computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+function getComputerChoice() {
+    //should be able to generate 3 choices
+    let choice = Math.floor(Math.random() * 3);
+    if (choice == 0) {
+        return "rock";
+    }
+    else if (choice == 1) {
+        return "paper";
+    }
+    return "scissors";
+}
